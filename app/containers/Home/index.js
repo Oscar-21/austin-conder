77d01403c64b3c341da25980e55035c384efe647
theClubhouse <heythere@theclubhou.se>
2017-03-22 15:50:54 -0400

    added nav bar

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -11,58 +11,51 @@ import {Link} from "react-router"; /* allow us to link to different pages in pro
 export default class Home extends React.PureComponent {
   render() {
 
-    const titleStyle = {
+    const mainStyle = {
         display: "flex",
         flexDirection: "column",
+        /*flexWrap: "no-wrap",*/
         /*padding: "13.5%",*/
         background:"url(https://68.media.tumblr.com/8ba6be52a6cb41fe964cd7fbb94a3e02/tumblr_on2pafGDUw1vieawno1_1280.jpg)",
         backgroundSize:"cover",
-        width:"100%",
+        maxWidth:"100%",
         height:"475px",
     }
 
-    const PageNamestyle = {
+    const navStyle = {
         display: "flex",
         flexDirection: "column",
 
-
     }
 
-    const titlePage = {
+    const currentIssue = {
         display: "flex",
-        color: "#eeeeee",
+        color: "#FFFFFF",
         justifyContent: "center",
-        fontSize: "5em",
+        fontSize: "4.1em",
         fontFamily: "Source Sans Pro",
         fontStyle: "light",
         fontWeight: "400",
         textTransform: "uppercase",
         letterSpacing: "6px",
         textAlign: "center",
-        marginTop: "200px"
+        marginTop: "160px"
         /*marginBottom: "160px"*/
 
     }
 
 
-    const screenshotStyle = {
+    const backgroundStyle = {
 
         display: "flex",
         justifyContent: "center",
         background: " #F5FEEF",
         height: "30vh",
-        paddingTop: "10%"
+        paddingTop: "10%",
+
         /* #B4CFEC*/
 
     }
-    /*const picBox = {
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        width: "13%",
-        height: "150px",
-        background: "red"
-    }*/
 
         const picBox = {
         display: "flex",
@@ -74,42 +67,42 @@ export default class Home extends React.PureComponent {
         paddingBottom: "2200px"
     }
 
-       const thumbNails = {
-
-            width: "100%",
-            height: "128px",
-            marginBottom: "255px"
+       const articleStyle = {
+            marginBottom: "255px",
         }
 
-        const placeHoldertext = {
+        const articleImg = {
+            width: "100%",
+            height: "120px",
+        }
+
+        const articleTitle = {
+            fontFamily: "Romantic",
+            fontSize: ".84em",
+            fontWeight: "bold"
+
+        }
+
+        const authorStyle = {
             display: "flex",
-            textAlign: "center",
-            justifyContent: "center",
-            fontFamily: "Merriweather",
-            color:"#000000",
+            flexDirection: "column",
+            fontFamily: "Romantic",
+            fontSize: ".63em",
+            fontWeight: "bold",
+            marginLeft: "100px"
+
+
         }
 
         const footStyle = {
             display: "flex",
             flexDirection: "column",
             background: "#0C090A",
-            height: "20vh",
-            width: "100%"
+            height: "22vh",
+            width: "100%",
+            /*marginLeft: "50px",
+            marginRight: "50px"*/
         }
-
-    /*
-        const thirdPic = {
-        width: "13%",
-        height: "150px",
-        background:"#0000ff"
-    }
-
-        const fourthPic = {
-        width: "13%",
-        height: "150px",
-        background:"#ff0000"
-    }*/
-
 
 
     return (
@@ -120,89 +113,120 @@ export default class Home extends React.PureComponent {
         </header>
 
         <main>
-            <div style = {titleStyle}>
+            <div style = {mainStyle}>
 
-                <div style = {PageNamestyle}>
-                    <div style = {{    color: "#eeeeee",
+                <div style = {navStyle}>
+                    <div style = {{
+                        color: "#eeeeee",
                         fontFamily: "Trebuchet MS",
                         fontStyle: "light",
                         fontWeight: "400",
                         fontSize: "2em",
                         textTransform: "uppercase",
-                        textAlign: "center"}}>word on reel weekly</div>
-                    <div style =     {{color: "#eeeeee",
-                        fontSize: "1em",
-                        fontFamily: "consolas",
+                        textAlign: "center",
+                        }}>
+                            word on reel weekly
+                    </div>
+
+
+                    <nav style = {{
+                        marginTop: "10px",
+                        color: "#eeeeee",
+                        fontSize: ".8em",
+                        fontFamily: "courier",
                         fontStyle: "light",
-                        fontWeight: "400",
                         textTransform: "uppercase",
-                        textAlign: "center"}}>your daily source for cinematic celebrations</div>
+                        textAlign: "center"
+                        }}>
+                        / current issue / about us / browse by topic / archive /
+                    </nav>
+
                 </div>
 
-                <div style = {titlePage}>
+                <div style = {currentIssue}>
                     Cinema, so blue...
                 </div>
 
             </div>
 
-            <div style = {screenshotStyle}>
+            <div style = {backgroundStyle}>
 
                 <div style={picBox}>
-                    <div>
-                        <img style = {thumbNails} src="https://68.media.tumblr.com/630ee7e6247d5f16efe7f315bff42707/tumblr_n3uidp4lD71ty9ma3o1_1280.jpg" alt=""/>
+                    <div style = {articleStyle}>
+                        <img style = {articleImg} src="
+                            https://68.media.tumblr.com/630ee7e6247d5f16efe7f315bff42707/tumblr_n3uidp4lD71ty9ma3o1_1280.jpg" alt=""/>
+
+                        <div style = {articleTitle}>
+                            Lorem ipsum dolor sit.
+                        </div>
+                        <div style = {authorStyle}>
+                            - - - Lorem Ipsum
+                        </div>
                     </div>
-                    <div style = {placeHoldertext}>
-                        <h1>
-                            <Link to= "/" style={{
-                            textDecoration:'none',
-                            fontFamily: 'Merriweather',
-                            color:'black'
-                        }}>Home</Link>
-                        </h1>
+
+
+                </div>
+
+
+
+                <div style={picBox}>
+                    <div style = {articleStyle}>
+                        <img style = {articleImg} src="https://68.media.tumblr.com/618291bc87361d807acd04604f0966d7/tumblr_n3uidp4lD71ty9ma3o3_1280.jpg" alt=""/>
+
+                        <div style = {articleTitle}>
+                            Lorem ipsum dolor sit.
+                        </div>
+
+                        <div style = {authorStyle}>
+                            - - - Lorem Ipsum
+                        </div>
+
                     </div>
                 </div>
 
 
                 <div style={picBox}>
-                    <div>
-                        <img style = {thumbNails} src="https://68.media.tumblr.com/618291bc87361d807acd04604f0966d7/tumblr_n3uidp4lD71ty9ma3o3_1280.jpg" alt=""/>
-                    </div>
-                    <div style = {placeHoldertext}>
-                        <h1>
-                            <Link to= "/About" style={{
-                            textDecoration:'none',
-                            fontFamily: 'Merriweather',
-                            color:'black'
-                            }}>About</Link>
-                        </h1>
-                    </div>
-                </div>
+                    <div style = {articleStyle}>
+                        <img style = {articleImg} src="https://68.media.tumblr.com/9ee7cb189d0abfe5a3168cf8b07ac066/tumblr_n3uidp4lD71ty9ma3o2_1280.jpg" alt=""/>
 
 
-                <div style={picBox}>
-                    <div>
-                        <img style = {thumbNails} src="https://68.media.tumblr.com/9ee7cb189d0abfe5a3168cf8b07ac066/tumblr_n3uidp4lD71ty9ma3o2_1280.jpg" alt=""/>
-                    </div>
-                    <div style = {placeHoldertext}>
-                        <h1>Tres</h1>
+                        <div style = { articleTitle }>
+                            Lorem ipsum dolor sit.
+                        </div>
+
+                        <div style = {authorStyle}>
+                            - - - Lorem Ipsum
+                        </div>
                     </div>
                 </div>
 
                 <div style={picBox}>
-                    <div>
-                        <img style = {thumbNails} src="https://68.media.tumblr.com/cbc5d0a14637c62e9a7c36bbf166286d/tumblr_n3uidp4lD71ty9ma3o4_1280.jpg" alt=""/>
-                    </div>
-                    <div style = {placeHoldertext}>
-                        <h1>Quatro</h1>
-                    </div>
+                    <div style = {articleStyle}>
+                        <img style = {articleImg}            src="https://68.media.tumblr.com/cbc5d0a14637c62e9a7c36bbf166286d/tumblr_n3uidp4lD71ty9ma3o4_1280.jpg" alt=""/>
 
+                        <div style = {articleTitle}>
+                            Lorem ipsum dolor sit.
+                        </div>
 
+                        <div style = {authorStyle}>
+                            - - - Lorem Ipsum
+                        </div>
+                    </div>
                 </div>
 
 
             </div>
 
             <div style = {footStyle}>
+
+                <div>
+                    
+                </div>
+
+                <div>
+
+                </div>
+
             </div>
         </main>
 
