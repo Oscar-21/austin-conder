a544f37be56a5322630b588ddcd03fcf94603d4f
theClubhouse <heythere@theclubhou.se>
2017-04-17 16:07:53 -0400

    finished styling for about, began mobile styling

--- app/containers/About/index.js
+++ app/containers/About/index.js
@@ -8,6 +8,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import ArticleStyle from 'components/ArticleStyle';
 import {Link} from "react-router";
+import Responsive from 'react-responsive';
 export default class About extends React.PureComponent {
 
 constructor(props){
@@ -74,6 +75,17 @@ constructor(props){
 
     }
 
+    const titleStyleMobile = {
+      fontFamily: "Source Sans Pro",
+      textAlign: "center",
+      fontColor: "#0C090A",
+      fontSize: "2em",
+      fontStyle: "light",
+      fontWeight: "400",
+      textTransform: "uppercase",
+      letterSpacing: "4px",
+
+    }
     const firstStyle = {
       display: "flex",
       flexDirection: "row",
@@ -82,10 +94,25 @@ constructor(props){
       justifyContent: "space-around"
     }
 
+    const firstStyleMobile = {
+      display: "flex",
+      flexDirection: "column",
+      maxWidth: "1000px",
+      margin: "0 auto",
+      justifyContent: "space-around"
+    }
     const imageStyle = {
       marginTop: "5%",
       width: "20%",
       height: "20%"
+    }
+
+
+    const imageStyleMobile = {
+      marginLeft: "17%",
+      marginTop: "5%",
+      width: "65%",
+      height: "65%"
     }
 
     const subheaderStyle = {
@@ -97,6 +124,14 @@ constructor(props){
 
     }
 
+    const subheaderStyleMobile = {
+
+      display: "flex",
+      flexDirection: "column",
+      marginTop: "4%",
+      marginLeft: "10%"
+    }
+
     const subheaderOne = {
       fontFamily: "Droid Serif",
       fontSize: "1.3em",
@@ -105,6 +140,23 @@ constructor(props){
       textTransform: "uppercase",
     }
 
+    const subheaderTwo = {
+      fontFamily: "Droid Serif",
+      fontSize: "1.3em",
+      fontWeight: "bold",
+      textTransform: "uppercase",
+
+      marginBottom: "3%",
+    }
+
+
+    const subheaderThree = {
+      fontFamily: "Droid Serif",
+      fontSize: "1.3em",
+      fontWeight: "bold",
+      marginBottom: "3%",
+      textTransform: "uppercase",
+    }
     const contentOne = {
       width: "85%",
       borderTop: "1px solid gray",
@@ -118,21 +170,66 @@ constructor(props){
           <header>
             <ArticleStyle/>
           </header>
+          <Responsive minDeviceWidth={1024}>
+            <main style={backgroundStyle}>
 
-      <div style={backgroundStyle}>
+              <div style={backgroundContentBox}>
+                <div style={titleBox}>
+                  <div style = {titleStyle}>
+                    About us
+                  </div>
+                </div>
+                <div style={firstStyle}>
+                  <img style={imageStyle} src="https://68.media.tumblr.com/21eed63e017188a2b5546c68063c370c/tumblr_ook5rge8VR1ubdqpuo1_250.jpg" alt=""/>
+                  <div style={subheaderStyle}>
+                    <div style={subheaderOne}> We love cinema</div>
 
-        <div style={backgroundContentBox}>
-          <main>
-            <body>
+                    <div style={contentOne}>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                  </div>
+                </div>
+
+                  <div style={firstStyle}>
+                  <img style={imageStyle} src="https://68.media.tumblr.com/26511dfa3b6cb8273b945f9cab951ee9/tumblr_ook9ejNdlv1ubdqpuo1_540.jpg" alt=""/>
+                  <div style={subheaderStyle}>
+                    <div style={subheaderTwo}> You love cinema too?</div>
+
+                    <div style={contentOne}>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                  </div>
+                </div>
+
+
+                  <div style={firstStyle}>
+                  <img style={imageStyle} src="https://68.media.tumblr.com/2812b695c746380469792f0854c56a70/tumblr_ookd8yAJJI1ubdqpuo1_500.jpg" alt=""/>
+                  <div style={subheaderStyle}>
+                    <div style={subheaderTwo}> We want community Article submissions</div>
+
+                    <div style={contentOne}>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                  </div>
+                </div>
+
+                <footer>
+                </footer>
+              </div>
+            </main>
+          </Responsive>
+          <Responsive maxDeviceWidth={1023}>
+          <main style={backgroundStyle}>
+            <div style={backgroundContentBox}>
               <div style={titleBox}>
-                <div style = {titleStyle}>
+                <div style = {titleStyleMobile}>
                   About us
                 </div>
               </div>
-              <div style={firstStyle}>
-                <img style={imageStyle} src="https://68.media.tumblr.com/21eed63e017188a2b5546c68063c370c/tumblr_ook5rge8VR1ubdqpuo1_250.jpg" alt=""/>
-                <div style={subheaderStyle}>
-                  <div style={subheaderOne}> For the Love of Celluloid</div>
+              <div style={firstStyleMobile}>
+                <img style={imageStyleMobile} src="https://68.media.tumblr.com/21eed63e017188a2b5546c68063c370c/tumblr_ook5rge8VR1ubdqpuo1_250.jpg" alt=""/>
+                <div style={subheaderStyleMobile}>
+                  <div style={subheaderOne}> We love cinema</div>
 
                   <div style={contentOne}>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -140,10 +237,10 @@ constructor(props){
                 </div>
               </div>
 
-                <div style={firstStyle}>
-                <img style={imageStyle} src="https://68.media.tumblr.com/26511dfa3b6cb8273b945f9cab951ee9/tumblr_ook9ejNdlv1ubdqpuo1_540.jpg" alt=""/>
-                <div style={subheaderStyle}>
-                  <div style={subheaderOne}> We know that you love cinema too, and we want your article contributions </div>
+                <div style={firstStyleMobile}>
+                <img style={imageStyleMobile} src="https://68.media.tumblr.com/26511dfa3b6cb8273b945f9cab951ee9/tumblr_ook9ejNdlv1ubdqpuo1_540.jpg" alt=""/>
+                <div style={subheaderStyleMobile}>
+                  <div style={subheaderTwo}> You love cinema too?</div>
 
                   <div style={contentOne}>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -151,14 +248,23 @@ constructor(props){
                 </div>
               </div>
 
-            </body>
-          </main>
 
-          <footer>
-          </footer>
+              <div style={firstStyleMobile}>
+              <img style={imageStyleMobile} src="https://68.media.tumblr.com/2812b695c746380469792f0854c56a70/tumblr_ookd8yAJJI1ubdqpuo1_500.jpg" alt=""/>
+              <div style={subheaderStyleMobile}>
+                <div style={subheaderTwo}> We want community Article submissions</div>
+
+                <div style={contentOne}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+              </div>
+                  </div>
+                <footer>
+                </footer>
+              </div>
+            </main>
+          </Responsive>
         </div>
-      </div>
-      </div>
     );
   }
 }
